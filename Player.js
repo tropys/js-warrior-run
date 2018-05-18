380ba26ea9332f3aa07926f47e9115a500ba7054
isak <isak.eriksson.extern@sartorius-stedim.com>
2018-05-18 13:39:03 +0200

    Level 4

--- Player.js
+++ Player.js
@@ -1,9 +1,26 @@
 class Player {
+  constructor() {
+    this.health = undefined;
+  }
+
+  status(warrior) {
+    const currentHealth = warrior.health();
+    const isTakingDamage = currentHealth < this.health;
+    const isHurt = warrior.health() < 20;
+    return {currentHealth, isTakingDamage, isHurt};
+  }
+
   playTurn(warrior) {
+    if (!this.health)
+      this.health = warrior.health();
+
+    const {currentHealth, isHurt, isTakingDamage} = this.status(warrior);
+    this.health = currentHealth;
+
     if (warrior.feel().isUnit())
       return warrior.attack();
 
-    if (warrior.health() < 20)
+    if (isHurt && !isTakingDamage)
       return warrior.rest();
 
     warrior.walk();
